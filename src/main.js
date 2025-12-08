285e13f32f231679457fbdd6bd76043db8d2c478
Arthur <pennywennyweed@gmail.com>
2025-12-08 16:55:14 +0200

    imported router and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
+import router from '../router'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+
+app.use(createBootstrap())
+app.use(router)
+
+app.mount('#app')
