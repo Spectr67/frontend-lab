71069653ccdbda69889f49466861a79e13d79123
Arthur <pennywennyweed@gmail.com>
2025-12-08 17:02:30 +0200

    refactored app vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
+<script>
+import { BNav, BNavItem } from 'bootstrap-vue-next'
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterLink,
+    RouterView,
+    BNav,
+    BNavItem,
+  },
+}
+</script>
+
 <template>
-  <span>hello World</span>
+  <div>
+    <BNav tabs justified>
+      <BNavItem to="/" active-class="active">Home</BNavItem>
+      <BNavItem to="/countryList/">Countries</BNavItem>
+      <BNavItem to="/countryItem/:name">Selected Country</BNavItem>
+      <BNavItem to="/signUp/">Sign UP</BNavItem>
+      <BNavItem to="/signIn/">Sign In </BNavItem>
+      <BNavItem to="/aboutUs/">About us</BNavItem>
+    </BNav>
+
+    <div class="view"><RouterView /></div>
+  </div>
 </template>
+
+<style scoped>
+.view {
+  padding-top: 50px;
+}
+</style>
